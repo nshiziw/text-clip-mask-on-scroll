16ec22396900ec0ca47f5b4e64f9ba7a8652fb64
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-02-05 12:57:40 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+"use client";
+import { useRef, useEffect } from "react";
+
+export default function Home() {
+  const container = useRef(null);
+  const stickyMask = useRef(null);
+
+  const initialMaskSize = 0.8;
+  const targetMaskSize = 30;
+  const easing = 0.15;
+  let easedScrollProgress = 0;
+
+  useEffect(() => {
+    requestAnimationFrame(animate);
+  }, []);
+
+  const animate = () => {
+    const maskSizeProgress = targetMaskSize * getScrollProgress();
+    stickyMask.current.style.webkitMaskSize =
+      (initialMaskSize + maskSizeProgress) * 100 + "%";
+    requestAnimationFrame(animate);
+  };
+
+  const getScrollProgress = () => {
+    const scrollProgress =
+      stickyMask.current.offsetTop /
+      (container.current.getBoundingClientRect().height - window.innerHeight);
+    const delta = scrollProgress - easedScrollProgress;
+    easedScrollProgress += delta * easing;
+    return easedScrollProgress;
+  };
+
+  return (
+    <main className="main">
+      <div ref={container} className="container">
+        <div ref={stickyMask} className="stickyMask">
+          <video autoPlay muted loop>
+            <source src="../public/nature.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </main>
+  );
+}
